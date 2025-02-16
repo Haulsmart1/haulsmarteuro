eb52734a76446b3670d0126581e11168c464bd43
Haulsmart1 <stuart@adrcarriers.net>
2025-02-16 19:42:37 +0000

    Initial commit - Fix background images and deployment issues

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Smart Logistics Solutions</h1>
+      <p>Website is under development.</p>
+    </div>
+  );
+}
+
+export default App;
+
